0408895235cd86377f9041a9806d5b15ae44a499
noormohammad20 <mdsaim204@gmail.com>
2022-04-08 21:41:19 +0600

    Add Google Sign using custom hook with on State Change

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import useFirebase from '../../hooks/useFirebase'
 
 const Login = () => {
+    const { signingWithGoogle } = useFirebase()
     return (
         <div>
             <h3>Please Login</h3>
             <div style={{ margin: "20px" }}>
-                <button>Login With Google</button>
+                <button onClick={signingWithGoogle}>Login With Google</button>
             </div>
             <form>
                 <input type="email" name="" id="" placeholder='enter your email' />
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,28 @@
 import { useState } from "react"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import app from "../firebase.init"
+
+
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
 
 const useFirebase = () => {
     const [user, setUser] = useState({})
+
+    const signingWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const user = result.user
+                setUser(user)
+                console.log(user)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+    return {
+        user,
+        signingWithGoogle
+    }
 }
+export default useFirebase
